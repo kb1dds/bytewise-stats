c955977e64e0e542e611596017a93777cb37b921
Michael Robinson <kb1dds@gmail.com>
2025-08-02 19:32:19 -0400

    If supplied prefix is too short, prepend it

--- from_nextbyte_distribution.c
+++ from_nextbyte_distribution.c
@@ -11,11 +11,15 @@
 
 #define MAX_WINDOW_SIZE 1024
 
+#define ANSI_COLORS
+
+unsigned char random_draw( unsigned int *counts, unsigned int *tc );
+
 unsigned int get_byte_distribution( char *index_path, unsigned char *window, int window_size, unsigned int *counts, int *fallback );
 
 int main( int argc, char *argv[] ){
   int i, j, k, fallback;
-  unsigned int count, counts[256], total_count, rv, window_size, cws;
+  unsigned int count, counts[256], total_count, current_count, rv, window_size, cws;
   unsigned char window[MAX_WINDOW_SIZE], byte;
 
   /* Seed for random */
@@ -32,6 +36,23 @@ int main( int argc, char *argv[] ){
   /* Grab prefix from stdin */
   scanf("%s",window);
 
+  total_count = strlen( window );
+#ifdef DEBUG
+  fprintf(stderr,"Characters supplied: %d\n",total_count);
+#endif
+
+  /* If prefix isn't long enough, prepend with random characters using the global histogram */
+  if( total_count < window_size ){
+    current_count = window_size - total_count;
+    
+    for( i = window_size-1; i >= current_count; i -- ){
+      window[i] = window[i-current_count];	    
+    }
+    get_byte_distribution( argv[1], NULL, window_size, counts, NULL );
+    for( i = current_count-1; i >= 0; i --) {
+      window[i] = random_draw(counts, NULL);
+    }
+  }
   window[window_size] = '\0';
 
   printf("%s", window);
@@ -44,28 +65,28 @@ int main( int argc, char *argv[] ){
     cws = get_byte_distribution( argv[1], window, window_size, counts, &fallback );
     
     /* Draw random character from this distribution */
-    for( total_count = 0, i = 0; i < 256; i ++ ){
-      total_count += counts[i];
-    }
-    rv = (unsigned int)( (double)rand() * (double) total_count / (double) RAND_MAX);
-    for( total_count = 0, byte = 255, i = 0; i < 256; i ++ ){
-      total_count += counts[i];
-      if( rv < total_count ){
-	byte = i;
-	break;
-      }	
-    }
+    byte = random_draw( counts, &total_count );
 
 #ifdef DEBUG
     fprintf(stderr,"Random: %u %u %d -> %x:%c\n", rv, total_count, RAND_MAX, byte, byte);
 #endif
 
     /* Send to stdout */
-    if(fallback)
-      printf("\e[1;31m");
+#ifdef ANSI_COLORS
+    i = (int)(255*((double)counts[byte]/(double)total_count));
+    i = (i<100)?100:i;
+    if(fallback){
+      printf("\e[38;2;%d;0;0m",255-i);
+    }
+    else{
+      printf("\e[38;2;%d;%d;%dm",i,i,i);
+    }
+#endif
     printf("%c", byte);
+#ifdef ANSI_COLORS
     if(fallback)
       printf("\e[0m");
+#endif
 
     /* Advance window */
     for( i = 1; i < window_size; i ++){
@@ -78,45 +99,72 @@ int main( int argc, char *argv[] ){
   }
 }
 
+/* Draw random character from this distribution */
+unsigned char random_draw( unsigned int *counts, unsigned int *tc ){
+  unsigned int total_count, current_count, i, rv;
+  unsigned char byte;
+  
+  for( total_count = 0, i = 0; i < 256; i ++ ){
+    total_count += counts[i];
+  }
+  rv = (unsigned int)( (double)rand() * (double) total_count / (double) RAND_MAX);
+  for( current_count = 0, byte = 255, i = 0; i < 256; i ++ ){
+    current_count += counts[i];
+    if( rv < current_count ){
+      byte = i;
+      break;
+    }	
+  }
+  if( tc != NULL )
+    *tc = total_count;
+  
+  return byte;
+}
+
+
 unsigned int get_byte_distribution( char *index_path, unsigned char *window, int window_size, unsigned int *counts, int *fallback ){
   char index_file[MAX_WINDOW_SIZE*3];
   unsigned char *window_ptr;
   FILE *ifp;
   int cws, i;
 
-  for( window_ptr = window, cws = window_size; cws > 2 ; window_ptr ++, cws -- ){
-    /* Construct index filename */
-    index_filename( index_file, index_path, window_ptr, cws );
+  if( window != NULL ){
+    for( window_ptr = window, cws = window_size; cws > 2 ; window_ptr ++, cws -- ){
+      /* Construct index filename */
+      index_filename( index_file, index_path, window_ptr, cws );
 
 #ifdef DEBUG
-    fprintf(stderr,"Trying index file (cws=%d) : %s ... ",cws,index_file);
+      fprintf(stderr,"Trying index file (cws=%d) : %s ... ",cws,index_file);
 #endif
     
-    if( (ifp = fopen(index_file, "rb")) != NULL){
-      /* Index file found; pull counts and exit */
+      if( (ifp = fopen(index_file, "rb")) != NULL){
+	/* Index file found; pull counts and exit */
 #ifdef DEBUG
-      fprintf(stderr,"found!\nRead %lu\n",fread(counts, (sizeof counts[0]), 256, ifp));
+	fprintf(stderr,"found!\nRead %lu\n",fread(counts, (sizeof counts[0]), 256, ifp));
 #else
-      fread(counts, (sizeof counts[0]), 256, ifp);
+	fread(counts, (sizeof counts[0]), 256, ifp);
 #endif
 	
-      fclose(ifp);
+	fclose(ifp);
 
 #ifdef DEBUG
-      for( i = 0; i < 256; i ++ )
-	fprintf(stderr,"%x:%c:%u ",i,i,counts[i]);
-      fprintf(stderr,"\n");
+	for( i = 0; i < 256; i ++ )
+	  fprintf(stderr,"%x:%c:%u ",i,i,counts[i]);
+	fprintf(stderr,"\n");
 #endif
-      *fallback = 0;
-      return cws;
-    }
+	if( fallback != NULL )
+	  *fallback = 0;
+	return cws;
+      }
 #ifdef DEBUG
-    fprintf(stderr,"not found\n");
+      fprintf(stderr,"not found\n");
 #endif    
+    }
   }
 
   /* Look for a global histogram */
-  *fallback = 1;
+  if( fallback != NULL )
+    *fallback = 1;
   index_filename( index_file, index_path, window_ptr, 0 );
 
 #ifdef DEBUG
